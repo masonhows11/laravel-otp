599b79a6adae70aa46841f4f577d1a6b6f620e9a
mason.hows11 <mason.hows11@gmail.com>
2023-05-18 21:42:27 +0330

    front template updated

--- resources/views/include_front/master.blade.php
+++ resources/views/include_front/master.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME')  }}</title>
-    @yield('title_front')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title_front')</title>
+
     @include('include_front.header_styles')
 </head>
 <body>
-
+@include('include_front.side_navbar')
 
 @yield('main_content_front')
 
