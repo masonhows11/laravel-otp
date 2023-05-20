4217ba7dc6f9b565f68af6c4911863d3813ffa4c
mason.hows11 <mason.hows11@gmail.com>
2023-05-20 20:09:48 +0330

    add home controller

---
+++ app/Http/Middleware/VerifiedUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class VerifiedUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $auth_user = DB::table('users')->where('email',Auth::user()->email)->first();
+        if( $auth_user->email_verified_at == null ){
+            return redirect()->route('login.form')->with('error','کاربر گرامی حساب کاربری شما فعال نشده است.');
+        }
+
+        return $next($request);
+    }
+}
