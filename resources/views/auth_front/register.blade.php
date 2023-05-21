06044d3574e1d437dae8be1b443ad6b8f9bbc08c
mason.hows11 <mason.hows11@gmail.com>
2023-05-21 19:50:43 +0330

    add mobileValidation rule

--- resources/views/auth_front/register.blade.php
+++ resources/views/auth_front/register.blade.php
@@ -16,12 +16,22 @@
                 <div class="col-xl-6 col-lg-6 col-md-6   border border-2  rounded-3 py-4 px-4 register-form">
                     <form action="{{ route('register') }}" method="post">
                         @csrf
+
                         <div class="mb-3 mt-3">
                             <label for="name" class="form-label">نام کاربری</label>
                             <input type="text" class="@error('name') is-invalid @enderror form-control" id="name"
                                    name="name" value="{{ old('name') }}">
                         </div>
                         @error('name')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+
+                        <div class="mb-3 mt-3">
+                            <label for="mobile" class="form-label">موبایل</label>
+                            <input type="text" class="@error('mobile') is_invalid @enderror form-control" id="mobile"
+                                   name="mobile" value="{{ old('mobile') }}">
+                        </div>
+                        @error('mobile')
                         <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
 
@@ -34,22 +44,8 @@
                         <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
 
-                        <div class="mb-3 mt-3">
-                            <label for="pwd" class="form-label">رمز عبور</label>
-                            <input type="password" class="@error('password') is-invalid @enderror form-control"
-                                   id="pwd" name="password">
-                        </div>
-                        @error('password')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                        @enderror
 
-                        <div class="mb-3 mt-3">
-                            <label for="pwd-confirm" class="form-label">تکرار رمز عبور</label>
-                            <input type="password" class="@error('password') is-invalid @enderror form-control"
-                                   id="pwd-confirm"
-                                   name="password_confirmation">
-                        </div>
-                        <button type="submit" class="btn btn-register  w3-flat-alizarin rounded-3">عضویت</button>
+                        <button type="submit" class="btn btn-register  w3-flat-alizarin rounded-3">ثبت نام</button>
                     </form>
                 </div>
 
