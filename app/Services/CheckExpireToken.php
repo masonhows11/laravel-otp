585a230606cea96e91baaecae3851ec21297a522
mason.hows11 <mason.hows11@gmail.com>
2023-05-22 20:06:33 +0330

    reg user updated

--- app/Services/CheckExpireToken.php
+++ app/Services/CheckExpireToken.php
@@ -13,18 +13,18 @@ use Illuminate\Support\Facades\Date;
 class CheckExpireToken
 {
 
-    public static function checkAdminToken($code,$mobile)
+    public static function checkAdminToken($token,$mobile)
     {
         try {
             $admin = User::where('mobile',$mobile)
-                ->where('code',$code)
+                ->where('token',$token)
                 ->first();
             $expired = Carbon::parse($admin->updated_at)->addMinutes(1)
                 ->isPast();
             if($expired){
                 return false;
             }
-            $admin->code_verified_at = Date::now();
+            $admin->token_verified_at = Date::now();
             $admin->save();
             return true;
         }catch (\Exception $ex){
