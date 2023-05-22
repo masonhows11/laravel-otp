3d3fa1a265eb4974e6474b031ff4db8b82274ab3
mason.hows11 <mason.hows11@gmail.com>
2023-05-22 21:28:01 +0330

    verfied user mobile updated

--- app/Http/Controllers/Auth/VerifyUserMobileController.php
+++ app/Http/Controllers/Auth/VerifyUserMobileController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Services\CheckExpireToken;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VerifyUserMobileController extends Controller
 {
@@ -17,6 +20,19 @@ class VerifyUserMobileController extends Controller
     public function verifiedMobile(Request $request)
     {
 
+        $expire = CheckExpireToken::checkAdminToken($request->token, $request->mobile);
+
+        if ($expire == false) {
+            return redirect()
+                ->route('verified.mobile.form')
+                ->with(['error' => 'کد فعالسازی معتبر نمی باشد.']);
+        }
+        if ($user = User::where(['mobile' => $request->mobile, 'token' => $request->token])->first()) {
+            Auth::login($user, $remember = true);
+            return redirect()->route('home');
+        }
+
+        return redirect()->route('login.form');
 
     }
 }
