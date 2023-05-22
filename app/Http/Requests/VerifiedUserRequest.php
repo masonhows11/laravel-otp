2cebab15de5c5ec27e686094002429793a775949
mason.hows11 <mason.hows11@gmail.com>
2023-05-22 21:33:23 +0330

    verfied user mobile updated

--- app/Http/Requests/VerifiedUserRequest.php
+++ app/Http/Requests/VerifiedUserRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Rules\MobileValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginUserRequest extends FormRequest
+class VerifiedUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -45,5 +45,4 @@ class LoginUserRequest extends FormRequest
             'token.digits' => 'کد فعال سازی معتبر نمی باشد.',
         ];
     }
-
 }
