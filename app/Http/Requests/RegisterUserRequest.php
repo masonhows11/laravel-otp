65fadb936e2f6e7c45bccf671d0f94dd67add959
mason.hows11 <mason.hows11@gmail.com>
2023-05-22 05:54:53 +0330

    config register request class

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\MobileValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterUserRequest extends FormRequest
@@ -24,7 +25,32 @@ class RegisterUserRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'unique:users', 'min:5', 'max:20'],
+            'email' => ['required', 'unique:users', 'email'],
+            'mobile' => ['required','unique:users',new MobileValidationRule()],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'نام کاربری الزامی است.',
+            'name.unique' => 'نام کاربری تکراری است.',
+            'name.min' => 'حداقل ۶ کاراکتر باشد.',
+            'name.max' => 'حداکثر ۲۰ کاراکتر باشد.',
+
+            'email.required' => 'ایمیل الزامی است.',
+            'email.unique' => 'ایمیل تکراری است.',
+            'email.email' => 'ایمیل معتبر نیست.',
+
+
+            'mobile.required' => 'شماره موبایل خود را وارد کنید.',
+            'mobile.unique' => 'شماره موبایل وارد شده تکراری است.',
         ];
     }
 }
