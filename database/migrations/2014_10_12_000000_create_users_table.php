f20aff95c8f955dbc349e34fecbc3e1c2dc8ba02
mason.hows11 <mason.hows11@gmail.com>
2023-05-22 19:03:06 +0330

    add token column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('mobile')->unique();
+            $table->string('token')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('token_verified_at')->nullable();
             $table->string('password');
